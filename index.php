55b0509e1154f1ffe5e7d79d850150e53ad72a5e
asp6f8 <apsynthes@gmail.com>
2017-12-05 19:29:33 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,55 @@
 <?php
-	function deleteTask() {
+	session_start();
+	
+	$message = '';
+	
+	$target = $_GET['target'];
+	$action = $_POST['action'];
+	$data = null;
+	
+	switch($action) {
+		case 'delete':
+			$message = deletePlaylist();
+			break;
+		case 'add':
+			list($target, $message, $data) = addPlaylist();
+			break;
+		case 'edit':
+			list($target, $message, $data) = editPlaylist();
+			break;
+		case 'update':
+			list($target, $message, $data) = updatePlaylist();
+	}
+
+	function addPlaylist() {
+		$message = '';
+		
+		if ($_POST['cancel']) {
+			$message = 'Adding new playlist was cancelled.';
+			return array('', $message);
+		}
+
+		// Create connection
+		require('db_credentials.php');
+		$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+		// Check connection
+		if ($mysqli->connect_error) {
+			$message = $mysqli->connect_error;
+		} else {
+			$sql = "INSERT INTO Playlist (title, description, category, addDate) VALUES ('$title', '$description', '$category', NOW())"; //needs modification to fit parameters of Playlist table
+	
+			if ($result = $mysqli->query($sql)) {
+				$message = "Playlist was added";
+			} else {
+				$message = $mysqli->error;
+			}
+
+		}
+		
+		return array('', $message);
+	}
+	function deletePlaylist() {
 		$id = $_POST['id'];
 	
 		$message = "";
@@ -17,7 +67,7 @@
 				$id = $mysqli->real_escape_string($id);
 				$sql = "DELETE FROM Playlist WHERE PlaylistID = $id";
 				if ( $result = $mysqli->query($sql) ) {
-					$message = "Task was deleted.";
+					$message = "Playlist was deleted.";
 				} else {
 					$message = $mysqli->error;
 				}
@@ -28,4 +78,6 @@
 		return $message;
 	}
 
+
+	//needs delete user function, update playlist, current functions need refining/rewriting
 ?>

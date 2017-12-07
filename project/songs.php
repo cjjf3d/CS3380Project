11a483570c5ac43ee935b1b909848b097a24c7e0
EC2 Default User <ec2-user@ip-172-31-28-66.us-east-2.compute.internal>
2017-12-07 03:53:10 +0000

    hi

--- project/songs.php
+++ project/songs.php
@@ -4,6 +4,9 @@ require('db_credentials_finalProj.php');
 		$mysqli = new mysqli($servername, $username, $password, $dbname) or die("Can't connect");
 
         $a = $mysqli->query("Select * from Song");
-        $b = $a->fetch_array();
+        $b = array();
+	while(($row = $a->fetch_assoc()) != null) {
+		array_push($b, $row);
+	}
         echo(json_encode ($b) );
 ?>

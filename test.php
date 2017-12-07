fc1bf969a2e5a658111d4f6a32eb8e96afa31974
Unknown <charles.j.johnson@hotmail.com>
2017-12-06 22:32:17 -0600

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 6e6570b7be5e4533555ebcd4415d9e18156cd81b.

--- test.php
+++ test.php
@@ -2,8 +2,6 @@
 
 require('db_credentials_finalProj.php');
 		$mysqli = new mysqli($servername, $username, $password, $dbname) or die("Can't connect");
+		echo("hello");
 
-        $a = $mysqli->query("Select * from Song");
-        $b = $a->fetch_array();
-        echo(json_encode ($b) );
 ?>

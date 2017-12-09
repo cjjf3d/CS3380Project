996b76181e333f5e30a64683cfb0fbcb5c80ea25
EC2 Default User <ec2-user@ip-172-31-28-66.us-east-2.compute.internal>
2017-12-09 00:36:47 +0000

    typo

--- project/make_account.php
+++ project/make_account.php
@@ -3,8 +3,7 @@ require('db_credentials_finalProj.php');
 $mysqli = new mysqli($servername, $username, $password, $dbname) or die("Can't connect");
 $result = $mysqli->query("INSERT INTO Users(UserName, Password) VALUES ('" . $_POST["username"] . "', '" . $_POST["password"] . "')");
 if ($result->affected_rows > 0) {
-    echo "<h3 style='margin:auto;'>Account Created!</h3>";
-    echo "<meta http-equiv='refresh' content='3; url=index.php'>"
+	header("Location: index.php");
 }
 else {
 
